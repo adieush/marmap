7e5b8b9e4c0be61fb23e82eba8be0607af762b83
Alexey Shevchenko <adieush@gmail.com>
2022-03-21 21:11:44 +0200

    Add GA

--- index.php
+++ index.php
@@ -3,6 +3,17 @@ $params = require __DIR__ . '/config/config.php';
 ?>
 <!DOCTYPE html>
 <html>
+<head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-38LF5KWF3G"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-38LF5KWF3G');
+    </script>
+</head>
 <body>
 
 <h3>Чаты Мариуполя</h3>
