3844102c70032022e783838a69dcb34204011219
Alexey Shevchenko <adieush@gmail.com>
2022-03-21 20:50:47 +0200

    Add time

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $params = require __DIR__ . '/config/config.php';
 
 <div id="googleMap" style="width:100%;height:600px;"></div>
 
-<script src="js/scripts.js"></script>
+<script src="js/scripts.js?t=<?=time()?>"></script>
 
 <script src="https://maps.googleapis.com/maps/api/js?key=<?=$params['googleMapsAPIKey']?>&callback=Main.myMap"></script>
 
